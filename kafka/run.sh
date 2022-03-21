7dd6ec1100f220d9bac554b340d3eed82970c95f
jmbeach <jaredbeachdesign@gmail.com>
2022-03-21 08:29:35 -0400

    fix mounting volume

--- kafka/run.sh
+++ kafka/run.sh
@@ -1,2 +1,2 @@
 #docker network create --subnet 192.168.2.0/24 kafka
-docker run --name kafka -v ./data:/root/data --network kafka --ip 192.168.2.2 --hostname kafka0 -p 127.0.0.1:29092:29092/tcp kafka
+docker run --name kafka -v $PWD/data:/root/data --network kafka --ip 192.168.2.2 --hostname kafka0 -p 127.0.0.1:29092:29092/tcp kafka
